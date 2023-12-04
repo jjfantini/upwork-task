ea7622c4e18cb7ba7a94404eb5520b8f146affad
stevenlee131377 <stevenlee131377@gmail.com>
2023-12-03 16:51:39 -0800

    ✨ successful

--- .cz-config.js
+++ .cz-config.js
@@ -16,7 +16,7 @@ module.exports = {
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
   types: [
-    { value: "[EMOJI_PLACEHOLDER] feat", name: "A new feature" },
+    { value: "✨ feat", name: "A new feature" },
     { value: "🐛 fix", name: "A bug fix" },
     { value: "🚑 hotfix", name: "A temporary hotfix" },
     {
