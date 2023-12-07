5c174447a8f8b8cd118a9f01aeb66b697b8a12b8
stevenlee131377 <stevenlee131377@gmail.com>
2023-12-06 20:42:22 -0800

    hotfix(tests): test

--- .cz-config.js
+++ .cz-config.js
@@ -14,7 +14,7 @@ module.exports = {
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
   types: [
-    { value: "feat", name: "A new feature" },
+    // { value: "feat", name: "A new feature" },
     { value: "fix", name: "A bug fix" },
     { value: "hotfix", name: "A temporary hotfix" },
   ],
