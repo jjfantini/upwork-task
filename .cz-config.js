d10c79660a5c63dca40482033cdfb4149d26abb8
stevenlee131377 <stevenlee131377@gmail.com>
2023-12-08 21:03:58 -0800

    feat(core): without emoji feat

--- .cz-config.js
+++ .cz-config.js
@@ -16,8 +16,8 @@ module.exports = {
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
   types: [
-    { value: "✨ feat", name: "A new feature" },
-    { value: "🐛 fix", name: "A bug fix" },
+    { value: " feat", name: "A new feature" },
+    { value: "🐛 fix✨", name: "A bug fix" },
     { value: "🚑 hotfix", name: "A temporary hotfix" },
     {
       value: "🔨 chore",
