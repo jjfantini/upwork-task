fefb705ea67563b8f0e46ab3fe293a900506d1c8
stevenlee131377 <stevenlee131377@gmail.com>
2023-12-08 22:36:11 -0800

    m 😀 feat (core): emoji with m

--- .cz-config.js
+++ .cz-config.js
@@ -16,7 +16,7 @@ module.exports = {
     confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
   types: [
-    { value: "mmm 😀 feat ", name: "A new feature" },
+    { value: "m 😀 feat ", name: "A new feature" },
     { value: "fix 😀", name: "A bug fix" },
     { value: "hotfix 😀", name: "A temporary hotfix" },
   ],
