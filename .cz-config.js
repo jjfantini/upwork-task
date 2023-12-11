cf66e961696321c5b56745d3ca7435b8c0a3f8c5
stevenlee131377 <stevenlee131377@gmail.com>
2023-12-10 20:37:09 -0800

    fix 🐛(core):

--- .cz-config.js
+++ .cz-config.js
@@ -19,7 +19,7 @@ module.exports = {
     { value: "feat ✨", name: "A new feature" }, //
     { value: "fix 🐛", name: "A bug fix" }, //🐛
 
-    { value: "hotfix 🚑", name: "A temporary hotfix" },
+    { value: "hotfix 🚑", name: "A temporary hotfix" }, //
     {
       value: "chore 🔨",
       name: "Other changes that don't modify src or test files",
